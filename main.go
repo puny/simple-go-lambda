d2a4d24ccd38d7df42e44fbeedddcbe12e7fd6d7
bianchi <puny428@nate.com>
2025-10-22 00:00:37 +0900

    main.go update

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(ctx context.Context) (string, error) {
 
-	return "Hello, AWS Lambda!", nil
+	// return "Hello, AWS Lambda!", nil
+	return fmt.Sprintf("Hello, AWS Lambda!"), nil
 }
 
 func main() {
